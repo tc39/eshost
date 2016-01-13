615ff1f8acbe02f5cd8803c4b6523043fa7196dd
André Bargull <andre.bargull@gmail.com>
2016-01-13 20:26:06 +0100

    Add runner for JavaScriptCore

---
+++ runtimes/jsc.js
@@ -0,0 +1,41 @@
+var $ = {
+  global: this,
+  createRealm(globals) {
+    globals = globals || {};
+
+    var realm;
+    run(this.file, function(newRealm) {
+      realm = newRealm;
+    });
+    realm.eval(this.source);
+    realm.$.source = this.source;
+
+    for(var glob in globals) {
+      realm.$.global[glob] = globals[glob];
+    }
+
+    return realm.$;
+  },
+  evalInNewRealm(code, globals, errorCb) {
+    if (typeof globals === 'function') {
+      errorCb = globals;
+      globals = {};
+    }
+
+    var $child = this.createRealm(globals);
+    $child.evalInNewScript(code, errorCb);
+  },
+  evalInNewScript(code, errorCb) {
+    try {
+      /* FIXME: `code` should be executed as a global script, not an eval script. */
+      this.global.eval(code);
+    } catch (e) {
+      if (errorCb) errorCb(e);
+    }
+  },
+  setGlobal(name, value) {
+    this.global[name] = value;
+  },
+  source: $SOURCE,
+  file: $FILE
+};
