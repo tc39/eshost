8fea2f9a9e13c236da2a70abb5ba36a7def8c550
Rick Waldron <waldron.rick@gmail.com>
2019-10-02 14:24:52 -0400

    QJS: Updated runtime and agent
    These changes come directly from QuickJS and have been tested locally with test262-harness.

--- runtimes/qjs.js
+++ runtimes/qjs.js
@@ -1,41 +1,4 @@
-/* QuickJS exposes a $262 object to its runtime */
-if (!$262) {
-  /*
-  These are implemented in run-test262.c:
-    print()
-    $262.detachArrayBuffer
-    $262.agent.*
-
-  Once QuickJS releases an updated version of run-test262,
-  this whole section will not be necessary.
-  */
-  var $262 = {
-    evalScript() {
-      throw new Test262Error('evalScript not yet supported.');
-    },
-    detachArrayBuffer() {
-      throw new Test262Error('detachArrayBuffer not yet supported.');
-    },
-    agent: {
-      start() {
-        throw new Test262Error('agent.start not yet supported.');
-      },
-      broadcast() {
-        throw new Test262Error('agent.broadcast not yet supported.');
-      },
-      getReport() {
-        throw new Test262Error('agent.getReport not yet supported.');
-      },
-      sleep() {
-        throw new Test262Error('agent.sleep not yet supported.');
-      },
-      monotonicNow() {
-        throw new Test262Error('agent.monotonicNow not yet supported.');
-      },
-    }
-  };
-}
-
+/* run-test262 exposes a $262 object, so no need to define it */
 $262.source = $SOURCE;
 $262.destroy = function() {};
 $262.IsHTMLDDA = function() {};
