dc19559c2dcb34618d8e600492ddfe18988c4639
SergiyIvan <serhii.ivanenko@oracle.com>
2020-01-14 17:18:46 +0200

    Initial add GraalJS

--- runtimes/graaljs.js
+++ runtimes/graaljs.js
@@ -1,25 +1,29 @@
-var $262 = {
-  global: globalThis,
-  gc() {
-    return gc();
-  },
-  createRealm(options) {
-    throw new Test262Error('$262.createRealm not yet supported.');
-  },
-  evalScript(code) {
-    throw new Test262Error('$262.evalScript not yet supported.');
-  },
-  getGlobal(name) {
-    return this.global[name];
-  },
-  setGlobal(name, value) {
-    this.global[name] = value;
-  },
-  destroy() { /* noop */ },
-  IsHTMLDDA() { return {}; },
-  source: $SOURCE,
-  get agent() {
-    throw new Test262Error('Agent not yet supported.');
+// No need to create $262 object as it is provided behind --experimental-options --js.test262-mode=true flags
+$262.createRealm = function(options) {
+  options = options || {};
+  options.globals = options.globals || {};
+  var realm = loadWithNewGlobal({ script: 'this', name: 'createRealm' });
+  realm.eval(this.source);
+  realm.$262.source = this.source;
+  realm.$262.destroy = function () {
+    if (options.destroy) {
+      options.destroy();
+    }
+  };
+  for(var glob in options.globals) {
+    realm.$262.global[glob] = options.globals[glob];
   }
+  return realm.$262;
 };
-
+$262.gc = function() {
+  throw new Test262Error('GC not yet supported.');
+};
+$262.getGlobal = function(name) {
+  return this.global[name];
+};
+$262.setGlobal = function(name, value) {
+  this.global[name] = value;
+};
+$262.destroy = function () { /* noop */ };
+$262.IsHTMLDDA = function() { return {}; };
+$262.source = $SOURCE;
