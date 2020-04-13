ff43f5d7ea1db5072a818c9b67fb5d634df13bec
Alexey Shvayka <shvaikalesh@gmail.com>
2020-04-14 01:12:46 +0300

    Remove IsHTMLDDA stub for JSC
    Real `$262.IsHTMLDDA` was implemented in https://trac.webkit.org/changeset/259587

--- runtimes/jsc.js
+++ runtimes/jsc.js
@@ -1,7 +1,6 @@
 /* JavaScriptCore exposes a $ & $262 object to its runtime */
 $262.source = $SOURCE;
 $262.destroy = function() {};
-$262.IsHTMLDDA = function() {};
 $262.getGlobal = function(name) {
   return this.global[name];
 };
