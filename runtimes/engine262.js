f34d984ebb3c408a93680ddad4f790277db7fe2d
devsnek <me@gus.host>
2020-08-26 09:13:39 -0500

    Update engine262.js

--- runtimes/engine262.js
+++ runtimes/engine262.js
@@ -1,52 +1,25 @@
-/* Engine262 exposes a "$" object to its runtime */
-/* Using globalThis["\x24"]; prevents overwrite by ConsoleAgent */
-var engine262 = globalThis["\x24"];
-var $262 = {
-  realm: null,
-  global: engine262.global,
-  gc() {
-    return engine262.gc();
-  },
-  createRealm(options) {
+{
+  const { createRealm } = $262;
+  $262.createRealm = (options) => {
     options = options || {};
     options.globals = options.globals || {};
 
-    var realm = engine262.createRealm();
+    const realm = createRealm();
+    realm.evalScript($262.source);
 
-    realm.evalScript(this.source);
-    realm.getGlobal = this.getGlobal;
-    realm.setGlobal = this.setGlobal;
-    realm.source = this.source;
-    realm.destroy = function () {
-      if (options.destroy) {
-        options.destroy();
-      }
-    };
-
-    for (var glob in options.globals) {
-      realm.global[glob] = options.globals[glob];
+    for (const glob in options.globals) {
+      realm.$262.global[glob] = options.globals[glob];
     }
-
-    this.realm = realm;
+    if (options.destroy) {
+      realm.$262.destroy = options.destroy;
+    }
 
     return realm;
-  },
-  evalScript(code) {
-    try {
-      (this.realm || engine262).evalScript(code);
-      return { type: 'normal', value: undefined };
-    } catch (e) {
-      return { type: 'throw', value: e };
-    }
-  },
-  detachArrayBuffer: engine262.detachArrayBuffer,
-  getGlobal(name) {
-    return this.global[name];
-  },
-  setGlobal(name, value) {
-    this.global[name] = value;
-  },
-  destroy() { /* noop */ },
-  IsHTMLDDA() { return {}; },
-  source: $SOURCE
-};
+  };
+  $262.getGlobal = (name) => $262.global[name];
+  $262.setGlobal = (name, value) => {
+    $262.global[name] = value;
+  };
+  $262.destroy = () => {};
+  $262.source = $SOURCE;
+}
