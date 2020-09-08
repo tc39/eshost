edd8c7ced90e548123340ca6e5e1f04f87ac9ba9
Rick Waldron <waldron.rick@gmail.com>
2020-09-08 10:24:47 -0400

    Runtimes: remove no-op IsHTMLDDA from qjs (QuickJS is now shipping an implementation)

--- runtimes/qjs.js
+++ runtimes/qjs.js
@@ -1,7 +1,6 @@
 /* run-test262 exposes a $262 object, so no need to define it */
 $262.source = $SOURCE;
 $262.destroy = function() {};
-$262.IsHTMLDDA = function() {};
 $262.gc = function() {
   throw new Test262Error('gc() not yet supported.');
 };
