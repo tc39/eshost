d0f60b49a03ec6848130fc583d3e311cc0c76c40
rwaldron <waldron.rick@gmail.com>
2021-04-28 12:05:34 -0400

    CI: update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+const jestConfig = {
   collectCoverage: true,
   collectCoverageFrom: [
     '<rootDir>/lib/**/*.js',
@@ -17,3 +17,19 @@ module.exports = {
   testPathIgnorePatterns: [],
   verbose: true,
 };
+
+if (!process.env.CI) {
+  jestConfig.collectCoverageFrom.push(
+    '!<rootDir>/lib/Server.js',
+    '!<rootDir>/lib/WebdriverAgent.js',
+    '!<rootDir>/lib/agents/BrowserAgent.js',
+    '!<rootDir>/lib/agents/chrome.js',
+    '!<rootDir>/lib/agents/edge.js',
+    '!<rootDir>/lib/agents/firefox.js',
+    '!<rootDir>/lib/agents/remote.js',
+    '!<rootDir>/lib/agents/safari.js'
+  );
+
+}
+
+module.exports = jestConfig;
