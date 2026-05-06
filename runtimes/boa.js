bef14bb316c0ab682c02e04f4d21f60857fbd53d
Linus Groh <mail@linusgroh.de>
2026-05-06 21:20:02 +0100

    Add Boa
    Sources:
    - https://github.com/tc39/eshost/pull/147
    - https://github.com/test262-fyi/eshost/blob/master/runtimes/boa.js
    
    Co-Authored-By: Philip Chimento <pchimento@igalia.com>

---
+++ runtimes/boa.js
@@ -0,0 +1,55 @@
+function print() {
+  console.log.apply({}, arguments);
+}
+
+var $262 = {
+  global: globalThis,
+  gc() {
+    return $boa.gc.collect();
+  },
+  createRealm(options) {
+    options = options || {};
+    options.globals = options.globals || {};
+
+    var realm = $boa.realm.create();
+    realm.$boa = $boa;
+    realm.console = console;
+
+    var keys = Object.keys(options.globals);
+    for (var i = 0; i < keys.length; i++) {
+      realm[keys[i]] = options.globals[keys[i]];
+    }
+
+    realm.eval(this.source);
+    realm.$262.source = this.source;
+    realm.$262.destroy = function () {
+      if (options.destroy) {
+        options.destroy();
+      }
+    };
+
+    return realm.$262;
+  },
+  evalScript(code) {
+    try {
+      (0, eval)(code);
+      return { type: "normal", value: undefined };
+    } catch (e) {
+      return { type: "throw", value: e };
+    }
+  },
+  getGlobal(name) {
+    return this.global[name];
+  },
+  setGlobal(name, value) {
+    this.global[name] = value;
+  },
+  destroy() {
+    /* noop */
+  },
+  /* No IsHTMLDDA */
+  source: $SOURCE,
+  get agent() {
+    throw new Error("agent.* not yet supported.");
+  },
+};
