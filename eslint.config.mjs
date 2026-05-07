d38591926d6f953d44cbfd21b7e21e1e0543de38
Linus Groh <mail@linusgroh.de>
2026-05-07 20:52:58 +0100

    Clean up ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,44 +1,24 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig([
-  globalIgnores(["test/fixtures/**/*.js"]),
+  globalIgnores([
+    "test/fixtures",
+    // Too messy for now
+    "runtimes/*.js",
+  ]),
   {
-    extends: compat.extends("eslint:recommended"),
-
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
     languageOptions: {
       globals: {
-        ...globals.browser,
         ...globals.jest,
-        ...globals.jasmine,
         ...globals.node,
-        Test262Error: true,
-        gc: true,
-        $SOURCE: true,
-        fixture: true,
-        sandbox: true,
-        sinon: true,
       },
-
-      ecmaVersion: 2021,
-      sourceType: "module",
-    },
-
-    rules: {
-      "no-console": [0],
-      "no-empty-pattern": [0],
     },
   },
+  eslintPluginPrettierRecommended,
 ]);
